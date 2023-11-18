309c1d27d1781e8f2389c923078a69b7b0a0498f
Daniel Glover <iamglover02@gmail.com>
2023-11-18 04:55:50 +0000

    initialized splash screen

---
+++ app/splash/page.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const SplashScreen: React.FC = () => {
+  return (
+    <div className='flex items-center justify-center bg-white h-screen w-full'>
+      <span className='text-4xl text-black'>Hello</span>
+    </div>
+  );
+};
+
+export default SplashScreen;

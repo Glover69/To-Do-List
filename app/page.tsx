5127b66ad17e0a24110509f2be40c3a2c6f9ddb6
Daniel Glover <iamglover02@gmail.com>
2023-11-28 03:24:52 +0000

    Updates #2

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Link href="/dashboard">
-      <span>Get started</span>
+      <span className='text-white'>Get started</span>
       </Link>
     </main>
   )
--- app/splash/page.tsx
+++ app/splash/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const SplashScreen: React.FC = () => {
   return (
     <div className='flex items-center justify-center bg-white h-screen w-full'>
-      <span className='text-4xl text-black'>Hello</span>
+      <span className='text-4xl text-white'>Hello</span>
     </div>
   );
 };

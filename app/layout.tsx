fc9190e1b87c38bcf75f24b62bf284aa5e46c630
Daniel Glover <iamglover02@gmail.com>
2023-11-28 12:05:47 +0000

    updates #3

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <link rel="manifest" href="/manifest.webmanifest" />
+        <meta name="pwa-demo" content="pwa-demo" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="To-Do App" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="mobile-web-app-capable" content="yes" />
       </Head>
       <body className={lexend.className}>{children}</body>
     </html>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+
+/** @type {import('next').NextConfig} */
+
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require('next-pwa')({
+ dest: 'public', 
+ register: true,
+ skipWaiting: true,
+ runtimeCaching
+})
+
+module.exports = withPWA({
+ // other configs
+ reactStrictMode: false
+})

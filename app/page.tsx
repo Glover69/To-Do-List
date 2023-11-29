7c41bbd13601247d0d7a2eb20de54f0c0242d127
Daniel Glover <iamglover02@gmail.com>
2023-11-29 18:06:28 +0000

    Notification test

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,27 @@
+'use client';
+
 import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Home() {
+
+  const handleNotificationClick = () => {
+    if ('Notification' in window) {
+      Notification.requestPermission().then((permission) => {
+        if (permission === 'granted') {
+          new Notification('Hello, this is a notification!');
+        }
+      });
+    }
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Link href="/dashboard">
       <span className='text-white'>Get started</span>
       </Link>
+
+      <button className='text-white' onClick={handleNotificationClick}>Notification</button>
     </main>
   )
 }
